4a35f7a1b05754a2812082b57003c9c993811040
Ali <ali_malik96@yahoo.com>
2019-03-29 01:53:46 -0400

    Completed Trilateration

--- app/src/main/java/com/example/sharn/safetui/Trilateration.java
+++ app/src/main/java/com/example/sharn/safetui/Trilateration.java
@@ -42,9 +42,20 @@ public class Trilateration extends AppCompatActivity {
         List<ScanResult> scan = wifi.getScanResults();
         wifi.startScan();
 
-        double first, second, third;
-        first = second = third = Double.MAX_VALUE;
+        /**
+         * Temporary router variables to store the closest three routers
+         */
 
+        RouterInfo first, second, third;
+        first = new RouterInfo("first", 0, Double.MAX_VALUE, 0);
+        second = new RouterInfo("first", 0, Double.MAX_VALUE, 0);
+        third = new RouterInfo("first", 0, Double.MAX_VALUE, 0);
+
+
+        /**
+         * Create router instances for both the 2.4G and 5G bands.
+         * Currently only have 5 routers so 5 router instances
+         */
         RouterInfo r1_2 = new RouterInfo("SafeT_WIFI1", 0, 0, 0);
         RouterInfo r1_5 = new RouterInfo("SafeT_WIFI1-5G", 0, 0, 0);
 
@@ -60,6 +71,12 @@ public class Trilateration extends AppCompatActivity {
         RouterInfo r5_2 = new RouterInfo("SafeT_WIFI5", 0, 0, 0);
         RouterInfo r5_5 = new RouterInfo("SafeT_WIFI5-5G", 0, 0, 0);
 
+
+        /**
+         * Use WiFi Manager to scan the area for all access points.
+         * Filter by "our" routers and add the information to the router instances
+         * NOTE: Need to add the latitude and longitude of the routers after they're placed
+         */
         ArrayList<RouterInfo> list = new ArrayList<>();
         RouterInfo [] close = new RouterInfo[3];
         double distance = 0;
@@ -69,9 +86,10 @@ public class Trilateration extends AppCompatActivity {
                 r1_2.freq = r.frequency;
                 r1_2.rssi = r.level;
                 r1_2.distance = distance;
+                list.add(r1_2);
             }
             if (r.SSID.equals(r1_5.getName())) {
-                distance = (0.647345414 * Math.pow(r.level, 4.6170922718)) - 1.229882689;
+                distance = (0.070862507 * Math.pow(r.level, 6.235952987)) - 0.207677978;
                 r1_5.freq = r.frequency;
                 r1_5.rssi = r.level;
                 r1_5.distance = distance;
@@ -81,9 +99,10 @@ public class Trilateration extends AppCompatActivity {
                 r2_2.freq = r.frequency;
                 r2_2.rssi = r.level;
                 r2_2.distance = distance;
+                list.add(r2_2);
             }
             if (r.SSID.equals(r2_5.getName())) {
-                distance = (0.647345414 * Math.pow(r.level, 4.6170922718)) - 1.229882689;
+                distance = (0.070862507 * Math.pow(r.level, 6.235952987)) - 0.207677978;
                 r2_5.freq = r.frequency;
                 r2_5.rssi = r.level;
                 r2_5.distance = distance;
@@ -93,9 +112,10 @@ public class Trilateration extends AppCompatActivity {
                 r3_2.freq = r.frequency;
                 r3_2.rssi = r.level;
                 r3_2.distance = distance;
+                list.add(r3_2);
             }
             if (r.SSID.equals(r3_5.getName())) {
-                distance = (0.647345414 * Math.pow(r.level, 4.6170922718)) - 1.229882689;
+                distance = (0.070862507 * Math.pow(r.level, 6.235952987)) - 0.207677978;
                 r3_5.freq = r.frequency;
                 r3_5.rssi = r.level;
                 r3_5.distance = distance;
@@ -105,9 +125,10 @@ public class Trilateration extends AppCompatActivity {
                 r4_2.freq = r.frequency;
                 r4_2.rssi = r.level;
                 r4_2.distance = distance;
+                list.add(r4_2);
             }
             if (r.SSID.equals(r4_5.getName())) {
-                distance = (0.647345414 * Math.pow(r.level, 4.6170922718)) - 1.229882689;
+                distance = (0.070862507 * Math.pow(r.level, 6.235952987)) - 0.207677978;
                 r4_5.freq = r.frequency;
                 r4_5.rssi = r.level;
                 r4_5.distance = distance;
@@ -117,47 +138,45 @@ public class Trilateration extends AppCompatActivity {
                 r5_2.freq = r.frequency;
                 r5_2.rssi = r.level;
                 r5_2.distance = distance;
+                list.add(r5_2);
             }
             if (r.SSID.equals(r5_5.getName())) {
-                distance = (0.647345414 * Math.pow(r.level, 4.6170922718)) - 1.229882689;
+                distance = (0.070862507 * Math.pow(r.level, 6.235952987)) - 0.207677978;
                 r5_5.freq = r.frequency;
                 r5_5.rssi = r.level;
                 r5_5.distance = distance;
             }
         }
 
+
+        /**
+         * Determine three routers with the shortest distance
+         */
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getDistance() < first) {
+            if (list.get(i).getDistance() < first.getDistance()) {
                 third = second;
                 second = first;
-                first = list.get(i).getDistance();
-            } else if (list.get(i).getDistance() < second) {
+                first = list.get(i);
+            } else if (list.get(i).getDistance() < second.getDistance()) {
                 third = second;
-                second = list.get(i).getDistance();
-            } else if (list.get(i).getDistance() < third) {
-                third = list.get(i).getDistance();
+                second = list.get(i);
+            } else if (list.get(i).getDistance() < third.getDistance()) {
+                third = list.get(i);
             }
         }
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getDistance() == first) {
-                close[0]  = list.get(i);
-            }
-        }
-
 
 
 //assuming elevation = 0
         double earthR = 6371;
-        double LatA = 38.831723;
-        double LonA = -77.31404;
-        double DistA = 0.0198;
-        double LatB = 38.831698;
-        double LonB = -77.313607;
-        double DistB = 0.0198;
-        double LatC = 38.831607;
-        double LonC = -77.313853;
-        double DistC = 0.0168;
+        double LatA = first.getLatitude();
+        double LonA = first.getLongitude();
+        double DistA = first.getDistance()/1000;
+        double LatB = second.getLatitude();
+        double LonB = second.getLongitude();
+        double DistB = second.getDistance()/1000;
+        double LatC = third.getLatitude();
+        double LonC = third.getLongitude();
+        double DistC = third.getDistance();
 
     /*
     using authalic sphere
